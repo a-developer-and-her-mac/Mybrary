71aad2b89720be3c771306f3fececa8cf0f1f477
a-developer-and-her-mac <64797765+a-developer-and-her-mac@users.noreply.github.com>
2023-01-02 10:06:05 -0800

    Feat: update routes, views, models

--- routes/books.js
+++ routes/books.js
@@ -2,6 +2,16 @@ const express = require('express');
 const Book = require('../models/book');
 const router = express.Router();
 const Author = require('../models/author');
+const multer = require('multer')
+const path = require('path');
+const uploadPath = path.join('public', Book.coverImageBasePath);
+const imageMimeTypes = ['images/jpeg', 'images/png', 'images/gifs'];
+const upload = multer({
+  dest: uploadPath,
+  fileFilter: (req, file, callback) => {
+    callback(null, imageMimeTypes.includes(file.mimetype))
+  }
+});
 
 //All books route
 router.get('/', async (req, res) => {
@@ -24,7 +34,13 @@ router.get('/new', async (req, res) => {
 
 // Create new book route
 router.post('/', async (req, res) => {
-  res.send('Create Book')
+  const book = new Book({
+    title: req.body.title,
+    author: req.body.author,
+    publishDate: new Date(req.body.publishDate),
+    pageCount: req.body.pageCount,
+    description: req.body.description
+  })
 });
 
 module.exports = router;
